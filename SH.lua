a1ba25fb60432effbb41fae5106efd9cb2246729
samuraa1 <artemivanisovlol@gmail.com>
2025-12-16 12:47:54 +0300

    Update SH.lua

--- SH.lua
+++ SH.lua
@@ -1,2 +1,2 @@
 -- Hello skidders
-loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Solara-Hub/c8644f0a7d7ec88d4ab27497573c399452ea95c4/SH.lua'))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/ae39bdd588059917a1a4c2ee81caf442e8bcea2f04e9d5705f245e24d38d342a/download"))()
