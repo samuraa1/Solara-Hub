aa2199b28f1ac2d1590e4905e15d06d37dd8cf23
samuraa1 <artemivanisovlol@gmail.com>
2026-01-12 18:17:16 +0300

    Update SH.lua

--- SH.lua
+++ SH.lua
@@ -1,2 +1,2 @@
 -- Hello skidders 
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/674d9ddb4e166e2c21a6f1045385db7b97177e7aa47a74441707c07405d38117/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/674d9ddb4e166e2c21a6f1045385db7b97177e7aa47a74441707c07405d38117/download"))()
