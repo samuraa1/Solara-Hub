dcf74e2f174128244399e8eb3ae39e0dcd719ae3
samuraa1 <artemivanisovlol@gmail.com>
2026-01-23 18:51:37 +0300

    Create SH-Changelogs.lua

---
+++ SH-Changelogs.lua
@@ -0,0 +1,36 @@
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/EnesXVC/librarys/main/SearchAndChangeLogLibrary/Source'))()
+
+Library:CreateChangelogDialog({
+    Title = "Solara Hub - Changelogs",
+    ChangelogText = [[
+        Version: 4.912
+        
+    ]],
+    Search = false,
+    NotifyLibrary = "Luna",
+    Notifications = true,
+    Altbutton = {
+        Name = "Join Discord",
+        Clipboard = "discord.gg/DPCKQRJmdF"
+    },
+    Notification = {
+        Title = "Changelog",
+        Content = "The changelog has been closed.",
+        Duration = 3,
+        Image = 10723346871
+    },
+    AltbuttonNotification = {
+        Success = {
+            Title = "Copied!",
+            Content = "Link copied to clipboard.",
+            Duration = 5,
+            Image = 10709798443
+        },
+        Failure = {
+            Title = "Error",
+            Content = "Clipboard not supported.",
+            Duration = 5,
+            Image = 10709799124
+        }
+    }
+})
