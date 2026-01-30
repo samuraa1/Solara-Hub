719138109f04994af05b0427de4583cec9a9cab5
samuraa1 <artemivanisovlol@gmail.com>
2026-01-30 09:06:08 +0300

    Update SH.lua

--- SH.lua
+++ SH.lua
@@ -1,3 +1,2 @@
 -- Hello Skidders
-getgenv().SCRIPT_KEY = "KEYLESS"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/674d9ddb4e166e2c21a6f1045385db7b97177e7aa47a74441707c07405d38117/download"))()
