c5efc8b3bbf90c9fe56a408420b7087643346806
samuraa1 <artemivanisovlol@gmail.com>
2026-05-11 18:06:33 +0300

    Update SH.lua

--- SH.lua
+++ SH.lua
@@ -1,4 +1,1 @@
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bd62885d9e567716859471f56d1d9896d9f1473190ed1ef566fddc9dc356b760/download"))()
-
--- feedback ui, btw
-loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Solara-Hub/refs/heads/main/fb_ui.lua'))()
