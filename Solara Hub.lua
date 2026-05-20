8152519bcd94531ec47606183853eae12b8e6def
samuraa1 <artemivanisovlol@gmail.com>
2026-05-20 09:37:44 +0300

    Update Solara Hub.lua

--- Solara Hub.lua
+++ Solara Hub.lua
@@ -6,4 +6,13 @@
 ███████║╚██████╔╝███████╗██║  ██║██║  ██║██║  ██║    ██║  ██║╚██████╔╝██████╔╝
 ╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝    ╚═╝  ╚═╝ ╚═════╝ ╚═════╝                                                                            
 ]]
+if identifyexecutor and identifyexecutor() == "Solara" then
+  local old = getrawmetatable
+  getrawmetatable = function(obj)
+      if obj == _G then
+          return nil
+      end
+      return old(obj)
+  end
+end
 loadstring(game:HttpGet('https://raw.githubusercontent.com/samuraa1/Solara-Hub/refs/heads/main/SH.lua'))()
