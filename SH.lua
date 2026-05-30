7b642b264a4df24ff16f308636b0e9b0fed0916e
samuraa1 <artemivanisovlol@gmail.com>
2026-05-31 00:14:29 +0300

    Update SH.lua

--- SH.lua
+++ SH.lua
@@ -1,10 +1,1 @@
-if identifyexecutor and identifyexecutor() == "Solara" then
-  local old = getrawmetatable
-  getrawmetatable = function(obj)
-      if obj == _G then
-          return nil
-      end
-      return old(obj)
-  end
-end
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/674d9ddb4e166e2c21a6f1045385db7b97177e7aa47a74441707c07405d38117/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/bd62885d9e567716859471f56d1d9896d9f1473190ed1ef566fddc9dc356b760/download"))()
